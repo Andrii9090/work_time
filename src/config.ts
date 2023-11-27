be5338066849c9554bd82913606c7a39987205c1
Andrii9090 <webdevua2017@gmail.com>
2023-11-27 20:00:10 +0100

    add sequelize config

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,4 @@ config()
 export const BOT_TOKEN = process.env.BOT_TOKEN || ''
 export const LOCALE = process.env.LOCALE || 'es-ES'
 export const TIME_ZONE = process.env.TIME_ZONE || 'Europe/Madrid'
+
